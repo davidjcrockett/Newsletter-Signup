a2b9cd8f83e4955f3a608abe37a0afc25925ec48
David J Crockett <crockett.davidj@gmail.com>
2022-11-05 21:48:35 -0700

    changed signup to index, added variables to collect from app.post(. /signup

--- app.js
+++ app.js
@@ -7,11 +7,19 @@ const app = express();
 
 let PORT = 4000;
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
     res.render("Homepage")
+})
+
+app.post('/signup', (req, res) => {
+    const {firstName, lastName, email } = req.body;
+    if(!firstName || !lastName || !email) {
+        res.redirect('/failure.html');
+        return;
+    }
 })
 
 app.listen(PORT, () => {
