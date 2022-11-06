37905b1c29d326b806f4c2b8a58f38c280af3250
David J Crockett <crockett.davidj@gmail.com>
2022-11-05 21:36:10 -0700

    Sorted into public folder, added copy to signup, failure, and sucess html pages

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const request = require('request');
+const path = require('path');
 
 const app = express();
 
 let PORT = 4000;
 
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
-    res.send("Homepage")
+    res.render("Homepage")
 })
 
 app.listen(PORT, () => {
